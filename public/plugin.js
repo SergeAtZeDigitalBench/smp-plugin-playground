e68c0520e4e27346db2fa135a14d0afad42b48af
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-04-27 08:58:42 +0100

    fix: init refactor

--- public/plugin.js
+++
@@ -1,9 +0,0 @@
-const runPlugin = (utils, data) => {
-  console.log("Plugin script: ", { utils, data });
-
-  return {
-    pluginInitialisation: (pluginUtils) => {
-      console.log("pluginInitialisation(pluginUtils): ", { pluginUtils });
-    },
-  };
-};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log("Main Script");
+document.getElementById("mediaPlayer").innerHTML = `<h1>Hello PubSub</h1>`;
